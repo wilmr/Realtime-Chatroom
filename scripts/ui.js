cdbda57bacb8fd9ad551cefa1375ea6ae0f499fe
Wilmer Martinez <50350162+wilmr@users.noreply.github.com>
2022-02-27 22:50:26 -0800

    Add files via upload

---
+++ scripts/ui.js
@@ -0,0 +1,26 @@
+// render chat templates to the DOM
+// clear list of chats when switch rooms
+class ChatUI {
+    constructor(list){
+      this.list = list;
+    }
+
+    clear(){
+        this.list.innerHTML = '';
+    }
+
+    render(data){
+        const when = dateFns.distanceInWordsToNow(
+            data.created_at.toDate(),
+            { addSuffix: true}
+        );
+      const html = `
+        <li class="list-group-item">
+          <span class="username">${data.username}</span>
+          <span class="message">${data.message}</span>
+          <div class="time">${when}</span>
+        </li>
+      `;
+      this.list.innerHTML += html;
+    }
+  }
